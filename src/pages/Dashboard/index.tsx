dd181f739fd5c273b61600b8cf96aa3e9b875c17
Ruan Lima <ruan.lima@dellead.com>
2021-09-29 16:46:19 -0300

    Create layout dasboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/github_logo.png';
+import logoImgTitle from '../../assets/github_logo_title.svg';
 
 import * as S from './styles';
 
 const Dashboard: React.FC = () => {
   return (
+    <>
+
+    <S.Img src={logoImg} />
+    <S.ImgTitle src={logoImgTitle} />
+
     <S.Title>Explore repositórios no Github</S.Title>
+
+    <S.Form>
+      <input placeholder={"Digite o nome do repositório"} />
+      <button type={"submit"}>Pesquisar</button>
+    </S.Form>
+
+    <S.Repositories>
+      <a href={"test"}>
+        <img src="https://avatars.githubusercontent.com/u/57442366?v=4"
+        alt="Ruan Lima"
+        />
+        <div>
+          <strong>ruanblima/BookFinder</strong>
+          <p>Busca de lisvros</p>
+        </div>
+
+        <FiChevronRight size={20} />
+
+      </a>
+
+      <a href={"test"}>
+        <img src="https://avatars.githubusercontent.com/u/57442366?v=4"
+        alt="Ruan Lima"
+        />
+        <div>
+          <strong>ruanblima/BookFinder</strong>
+          <p>Busca de lisvros</p>
+        </div>
+
+        <FiChevronRight size={20} />
+
+      </a>
+
+      <a href={"test"}>
+        <img src="https://avatars.githubusercontent.com/u/57442366?v=4"
+        alt="Ruan Lima"
+        />
+        <div>
+          <strong>ruanblima/BookFinder</strong>
+          <p>Busca de lisvros</p>
+        </div>
+
+        <FiChevronRight size={20} />
+
+      </a>
+    </S.Repositories>
+
+
+
+    </>
   );
 };
 
