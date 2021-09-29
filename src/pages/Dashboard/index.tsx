ea46ceea17dd59e35a4b0080a5d6758890f8d86a
Ruan Lima <ruan.lima@dellead.com>
2021-09-29 17:16:42 -0300

    Connection aplication with api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,68 +1,75 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/github_logo.png';
 import logoImgTitle from '../../assets/github_logo_title.svg';
 
+import api from '../../services/api'
+
 import * as S from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+    ): Promise<void> {
+
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo("");
+  }
+
   return (
     <>
-
     <S.Img src={logoImg} />
     <S.ImgTitle src={logoImgTitle} />
 
     <S.Title>Explore repositórios no Github</S.Title>
 
-    <S.Form>
-      <input placeholder={"Digite o nome do repositório"} />
+    <S.Form onSubmit={handleAddRepository}>
+      <input
+        value={newRepo}
+        onChange={(e) =>
+        setNewRepo(e.target.value)}
+        placeholder={"Digite o nome do repositório"}
+      />
       <button type={"submit"}>Pesquisar</button>
     </S.Form>
 
     <S.Repositories>
-      <a href={"test"}>
-        <img src="https://avatars.githubusercontent.com/u/57442366?v=4"
-        alt="Ruan Lima"
+      {repositories.map(repository => (
+        <a key={repository.full_name} href={"test"}>
+        <img
+          src={repository.owner.avatar_url}
+          alt={repository.owner.login}
         />
         <div>
-          <strong>ruanblima/BookFinder</strong>
-          <p>Busca de lisvros</p>
+          <strong>{repository.full_name}</strong>
+          <p>{repository.description}</p>
         </div>
 
         <FiChevronRight size={20} />
 
       </a>
+      ))}
 
-      <a href={"test"}>
-        <img src="https://avatars.githubusercontent.com/u/57442366?v=4"
-        alt="Ruan Lima"
-        />
-        <div>
-          <strong>ruanblima/BookFinder</strong>
-          <p>Busca de lisvros</p>
-        </div>
-
-        <FiChevronRight size={20} />
-
-      </a>
-
-      <a href={"test"}>
-        <img src="https://avatars.githubusercontent.com/u/57442366?v=4"
-        alt="Ruan Lima"
-        />
-        <div>
-          <strong>ruanblima/BookFinder</strong>
-          <p>Busca de lisvros</p>
-        </div>
-
-        <FiChevronRight size={20} />
-
-      </a>
     </S.Repositories>
-
-
-
     </>
   );
 };
